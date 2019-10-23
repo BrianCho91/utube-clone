4c843f24181e49258dddb533a86f049a91e0dd1f
Brian Cho <jborame@gmail.com>
2019-10-23 16:03:45 -0700

    added resume and fixed github/linekdin links

--- frontend/components/header/sidebar.jsx
+++ frontend/components/header/sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faFire, faFolder, faLayerGroup } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faFire, faFolder, faLayerGroup, faFilePdf } from '@fortawesome/free-solid-svg-icons'
 import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 class Sidebar extends React.Component {
@@ -21,15 +21,25 @@ class Sidebar extends React.Component {
             </Link>
 
             <div className="sidebar-container-icon">
-              <FontAwesomeIcon className="faIcons" icon={faLinkedin} />
-              <p>LinkedIn</p>
+              <a href='https://www.linkedin.com/in/briancho91' target="_blank" className="sidebar-container-icon">
+                <FontAwesomeIcon className="faIcons" icon={faLinkedin} />
+                <p>LinkedIn</p>
+              </a>
             </div>
 
             <div className="sidebar-container-icon">
-              <FontAwesomeIcon className="faIcons" icon={faGithubSquare} />
-              <p>GitHub</p>
+              <a href='https://github.com/BrianCho91' target="_blank" className="sidebar-container-icon">
+                <FontAwesomeIcon className="faIcons" icon={faGithubSquare} />
+                <p>GitHub</p>
+              </a>
             </div>
 
+            <div className="sidebar-container-icon">
+              <a href='http://www.briancho.me/assets/documents/Resume.pdf' target="_blank" className="sidebar-container-icon">
+                <FontAwesomeIcon className="faIcons" icon={faFilePdf} />
+                <p>Resume</p>
+              </a>
+            </div>
           </div>
         </div>
       </div>
